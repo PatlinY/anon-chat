cd6dbabb96dd2434582ceb9cbd4366add26b7239
Yaroslav <bobob99639@gmail.com>
2025-12-17 18:47:27 +0300

    new-adress-2

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from '@elysiajs/eden';
 import type { App } from '@/app/api/[[...slugs]]/route';
 
-export const client = treaty<App>('http://localhost:3000').api;
+export const client = treaty<App>('https://anon-chat-kkw2wfhl4-yaroslavs-projects-9073d2e5.vercel.app').api;
